2794572e8d484194d9018919a428181b018b5b24
Miikka Solonen <K9924@student.jamk.fi>
2017-01-29 01:19:04 +0200

    toinenyritys

--- Poikkeukset/Program.cs
+++ Poikkeukset/Program.cs
@@ -1,15 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Poikkeukset
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Stringi joka etsii oikean polun
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            //uusi lista
+            List<string> persons = new List<string>();
+            //Luodaan uusi StreamWriter Olio jolla on parametrinä testitiedoston polku
+            StreamWriter outputFile = new StreamWriter(mydocpath + @"\test.txt");
+            
+            bool result = true;
+
+            while (result)
+            {
+
+                Console.Write("Anna nimiä ja halutessassi lopettaa kirjoita 'LOPETA' ");
+                string line = Console.ReadLine();
+
+
+
+                if (line == "LOPETA")
+                {
+                    result = false;
+                }
+                else
+                {
+                    persons.Add(line);
+                }
+
+            }
+            //kokeillaan foreachia ja otetaan koppia erroreista
+            try
+            {
+
+                foreach (string nimi in persons)
+                {
+                    outputFile.WriteLine(nimi.ToString());
+                }
+               
+
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Can't open file for writing (UnauthorizedAccessException)");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Opened stream is null (ArgumentNullException)");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Opened stream is not writable (ArgumentException)");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("An IO error happend (IOException)");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some other exception happend (Exception)");
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+            try{
+                string text = System.IO.File.ReadAllText(mydocpath + @"\test.txt");
+                System.Console.WriteLine("Contents of test.txt:\n" + text);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
         }
     }
 }
